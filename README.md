669eaf2a82aa8fa2f346ceca985fd5da94db17ed
Felipe Vilches <felovilches@gmail.com>
2021-09-09 05:29:39 +0900

    export npm package correctly

---
+++ README.md
@@ -0,0 +1,21 @@
+# Collection Sync
+
+(quick description)
+
+## Use cases
+
+## Install
+
+## How to use
+
+### Classes to implement
+
+### Syncing two databases
+
+### Syncing local database with remote database
+
+## Develop
+
+```bash
+npm run test:watch
+```
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import IInitializable from "./IInitializable";
+import SynchronizableCollection from "./SynchronizableCollection";
+import CollectionSyncMetadata from "./CollectionSyncMetadata";
+import CollectionItem from "./CollectionItem";
+import Collection from "./Collection";
+
+export {
+  IInitializable,
+  SynchronizableCollection,
+  CollectionSyncMetadata,
+  CollectionItem,
+  Collection
+};
