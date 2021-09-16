ab51734b93625d990bcf909e67dce3c34b1e43c8
Felo Vilches <felovilches@gmail.com>
2021-09-16 19:21:48 +0900

    pre hook test created (one)

--- test/real-use-cases/hookExamples.test.ts
+++ test/real-use-cases/hookExamples.test.ts
@@ -1,17 +1,39 @@
-/**
- * In order to test hooks, create a class (that extends an already made collection),
- * then do something simple like "before executing, copy the items to sync to a local object",
- * (items to sync are in the Synchronizer object, and "local" means SynchronizableCollection object),
- * that's enough to do something with the items.
- */
+import { SyncConflictStrategy, SyncOperation } from "../../src";
+import PersonItem from "../../src/example-implementations/PersonItem";
+import SynchronizableArray from "../../src/example-implementations/SynchronizableArray";
+import Synchronizer from "../../src/Synchronizer";
+import DocId from "../../src/types/DocId";
+
+function makeItem(id: DocId, date: string): PersonItem {
+  return new PersonItem(id, { name: "a", age: 20 }, new Date(date));
+}
+
+class Collection extends SynchronizableArray {
+  syncIds: DocId[] = [];
+
+  async preExecuteSync(synchronizer: Synchronizer): Promise<boolean> {
+    this.syncIds = synchronizer.itemsToSync.map(i => i.id);
+    return true;
+  }
+}
 
 describe("pre execute hook", () => {
-  xtest("before synchronizing, do something with with the items to sync", () => {
-
+  test("before synchronizing, it stores IDs to sync", async () => {
+    const slave = new Collection();
+    const master = new Collection();
+    slave.parent = master;
+    master.syncBatch([makeItem(10, "2020/05/10")]);
+    expect(slave.syncIds).toHaveLength(0);
+    await slave.sync(SyncOperation.Fetch, 10, { conflictStrategy: SyncConflictStrategy.RaiseError });
+    expect(slave.syncIds).toHaveLength(1);
   });
 
   xtest("before synchronizing, check that items haven't been pushed yet to the database", () => {
     // TODO: May need to implement populating upsertedItems in the Synchronizer class (if it's not already implemented).
+  });
+
+  xtest("if hook return is false, sync is stopped", () => {
+
   });
 });
 
